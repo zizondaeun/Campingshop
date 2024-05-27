91963bc4efda695e74429e91966deeb61314a02c
daeun <ekdms7059@naver.com>
2024-05-27 10:23:05 +0900

    5/27프론트 컨트롤 deletemywish.do 다시 수정

--- src/main/java/com/camcam/mypage/command/MyOrderListControl.java
+++ src/main/java/com/camcam/mypage/command/MyOrderListControl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.camcam.common.Control;
 import com.camcam.mypage.service.MyPageService;
@@ -18,9 +19,13 @@ public class MyOrderListControl implements Control {
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String path = "mypage/myorderList.tiles";
 		String id = req.getParameter("userId");
-	
+		if(id.equals("")) {
+			HttpSession session = req.getSession();
+			id = (String) session.getAttribute("logId");
+		}
+		
 		MyPageService svc = new MyPageServiceImpl();
-		List<OrderVO> list = svc.getMyorder("user01");
+		List<OrderVO> list = svc.getMyorder(id);
 		System.out.println(list);
 		req.setAttribute("list", list);
 		

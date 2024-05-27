8646a6f4ff3eb04ea9c93827ef8ea4321299d6d5
Jinhyoung Kim <jinhyoung0213@gmail.com>
2024-05-27 15:41:29 +0900

    장바구니 시작 commit

--- src/main/java/com/camcam/cart/mapper/CartMapper.java
+++ src/main/java/com/camcam/cart/mapper/CartMapper.java
@@ -1,5 +1,14 @@
 package com.camcam.cart.mapper;
 
-public interface CartMapper {
+import java.util.List;
 
+import com.camcam.cart.vo.CartVO;
+
+public interface CartMapper {
+	// 카트 목록
+	List<CartVO> selectCartList();
+	// 카트 수정
+	int updateCart(CartVO cvo);
+	// 카트 삭제
+	int deleteCart(int productNo);
 }

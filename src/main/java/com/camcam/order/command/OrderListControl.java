0d594fbe0f21936de955f7e5b03d4f380137d980
Jinhyoung Kim <jinhyoung0213@gmail.com>
2024-05-30 09:51:32 +0900

    마이바티스 컨피스 수정

--- src/main/java/com/camcam/order/command/OrderListControl.java
+++ src/main/java/com/camcam/order/command/OrderListControl.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.camcam.common.Control;
 
@@ -12,8 +13,14 @@ public class OrderListControl implements Control {
 
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 
+		HttpSession session = req.getSession();
+		
+		String userId = (String)session.getAttribute("logId");
+		String productNo = req.getParameter("pno");
+		
+		String[] productNoList = productNo.split(",");
+		
 	}
 
 }

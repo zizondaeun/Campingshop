871d4c9749aa7dc8bded8f51334fc30a0c20127b
LuciMer25 <1997@seokwon.com>
2024-05-31 17:43:46 +0900

    배송관리(완), 상품목록(수정중)

--- src/main/java/com/camcam/web/FrontController.java
+++ src/main/java/com/camcam/web/FrontController.java
@@ -62,6 +62,7 @@ import com.camcam.product.command.AddProductFormControl;
 import com.camcam.product.command.AddWishControl;
 import com.camcam.product.command.ProductInfoControl;
 import com.camcam.product.command.ProductListControl;
+import com.camcam.product.command.ProductListFormControl;
 import com.camcam.reply.command.AddReplyControl;
 import com.camcam.reply.command.ModReplyControl;
 import com.camcam.reply.command.RemoveReplyControl;
@@ -165,7 +166,8 @@ public class FrontController extends HttpServlet {
 		// 제품 관련(관리자)
 		map.put("/addProductForm.do", new AddProductFormControl()); // 상품 등록화면
 		map.put("/addProduct.do", new AddProductControl());
-
+		map.put("/ProductListForm.do", new ProductListFormControl());
+		
 		// 공지사항
 		map.put("/ListnoticePage.do", new ListnoticeControl()); // 공지사항 리스트
 		map.put("/noticePage.do", new noticeControl()); // 공지사항 상세화면 이동
@@ -210,7 +212,6 @@ public class FrontController extends HttpServlet {
 		map.put("/getTotalCnt.do", new TotalCountControl()); 
 		map.put("/modReply.do", new ModReplyControl());
 		
-
 		// 배송관리
 		map.put("/deliveryManage.do", new DeliManageControl());
 		map.put("/deliveryState.do", new DeliveryStatusControl());
--- src/main/webapp/WEB-INF/view/admin/productList.jsp
+++ src/main/webapp/WEB-INF/view/admin/productList.jsp
@@ -1,42 +1,20 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
-
+    pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
 <!DOCTYPE html>
 <html>
-<style>
-h3 {
-	padding-left: 50px;
-}
-
-.center {
-	margin-top: 30px;
-	padding-bottom: 50px;
-}
-
-.row {
-	padding-left: 70px;
-}
-
-.trhead, tbody tr {
-	text-align: center;
-}
-</style>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
 <body>
-	<h3>유저목록</h3>
 	<table class="table">
 		<thead>
 			<tr class="trhead">
-				<th>유저ID</th>
-				<th>이름</th>
-				<th>연락처</th>
+				<th>상품 카테고리</th>
+				<th>상품명</th>
+				<th>할인적용가격</th>
 				<th>이메일</th>
-				<th>가입일</th>
 			</tr>
 		</thead>
 		<tbody>

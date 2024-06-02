fe7f74280a7b28499d0add8a42c7d85e12ea58a1
ssh <ssh@DESKTOP-UHCKKVL>
2024-06-02 18:02:06 +0900

    notice 최종

--- src/main/java/com/camcam/notice/command/AddnoticeControl.java
+++ src/main/java/com/camcam/notice/command/AddnoticeControl.java
@@ -5,15 +5,48 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.camcam.board.service.BoardService;
+import com.camcam.board.service.impl.BoardServiceImpl;
+import com.camcam.board.vo.QnAVO;
 import com.camcam.common.Control;
+import com.camcam.notice.service.NoticeService;
+import com.camcam.notice.service.impl.NoticeServiceImpl;
+import com.camcam.notice.vo.NoticeVO;
 
 public class AddnoticeControl implements Control {
 
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		HttpSession session = req.getSession();
+		String userId = req.getParameter("userId");
+		String noticeSubject = req.getParameter("noticeSubject");
+		String noticeContent = req.getParameter("noticeContent");
+		
+		System.out.println(userId + noticeSubject + noticeContent + "************************************");
+		
+//		if(title == "" || content1 == "" || pw == "") {
+//			req.setAttribute("error", "값을 입력해주세요.");
+//			req.getRequestDispatcher("board/addBoard.tiles").forward(req, resp);
+//			return;
+//		}
+		
+		NoticeService svc = new NoticeServiceImpl();
 
+		NoticeVO vo = new NoticeVO();
+		vo.setUserId(userId);
+		vo.setNoticeSubject(noticeSubject);
+		vo.setNoticeContent(noticeContent);
+		
+		req.setAttribute("result", vo);
+		if(svc.addNotice(vo)) {
+			resp.sendRedirect("ListnoticePage.do");
+		}else {
+			resp.sendRedirect("error.do");
+				}
+		}
 	}
 
-}
+
--- src/main/java/com/camcam/notice/command/UpdatenoticeControl.java
+++ src/main/java/com/camcam/notice/command/UpdatenoticeControl.java
@@ -2,41 +2,50 @@ package com.camcam.notice.command;
 
 import java.io.IOException;
 
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.camcam.board.service.BoardService;
 import com.camcam.board.service.impl.BoardServiceImpl;
 import com.camcam.board.vo.QnAVO;
 import com.camcam.common.Control;
+import com.camcam.notice.service.NoticeService;
+import com.camcam.notice.service.impl.NoticeServiceImpl;
+import com.camcam.notice.vo.NoticeVO;
 
 public class UpdatenoticeControl implements Control {
 
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("utf-8");
-		
-		String bno = req.getParameter("bno");
-		String id = req.getParameter("userId");
-		String noticeSubject = req.getParameter("noticeSubject");
-		String noticecontent = req.getParameter("content").replaceAll("<[^>]*>", "");
+	
 
+	
 		
-		BoardService svc = new BoardServiceImpl();
-		QnAVO vo = new QnAVO();
-		vo.setBoardNo(Integer.parseInt(bno));
-		vo.setUserId(id);
-		vo.setTitle(noticeSubject);
-		vo.setContent(noticecontent);
+		String noticeNo = req.getParameter("noticeNo");
+		String noticeSubject = req.getParameter("noticeSubject");
+		String noticeContent = req.getParameter("noticeContent");
+		
+		System.out.println("*&&"+noticeNo + noticeSubject + noticeContent +" @@@@@@@@@@@@@@@@@@@@");
+		
+		NoticeService svc = new NoticeServiceImpl();
+
+		NoticeVO vo = new NoticeVO();
+		
+		vo.setNoticeNo(Integer.parseInt(noticeNo));
+		vo.setNoticeSubject(noticeSubject);
+		vo.setNoticeContent(noticeContent);
 		
 		req.setAttribute("result", vo);
 		//String encodeKW = URLEncoder.encode(kw, "UTF-8");
-		if(svc.modifyBoard(vo)) {
-			resp.sendRedirect("boardList.do");
+		if(svc.modifyNotice(vo)) {
+			resp.sendRedirect("ListnoticePage.do");
 		}else {
-			resp.sendRedirect("modBoardForm.do");
-		}	
+			
+		}
 	}
 
 }
